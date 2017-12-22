f40c5795f3649c8fc7e51f243da118f417fc33a1
Brian Hulette <hulettbh@gmail.com>
2017-12-22 00:44:35 -0500

    add day 18/67 solutions

--- 067/sol.py
+++ 067/sol.py
@@ -3,10 +3,14 @@ import sys
 with open('triangle.txt', 'r') as fp:
     triangle = [list(map(int, line.strip().split(' '))) for line in fp]
 
+# iterate from the bottom up. From each node, decide if it would be better to
+# go left or right, then sum that value
+# repeat all the way up...
 for r in range(len(triangle) - 2, -1, -1):
     row = triangle[r]
     below = triangle[r+1]
     for x in range(len(row)):
         row[x] += max(below[x], below[x+1])
 
+# ... then the answer is at the top
 print(triangle[0][0])
